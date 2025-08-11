68758b5ee0c1dfcb7026aff8c541974189097ef9
PhaniSaich <69901030+PhaniSaich@users.noreply.github.com>
2025-08-11 22:01:04 +0530

    Update p1.py

--- practise/p1.py
+++ practise/p1.py
@@ -1,1 +1,15 @@
+a = 5
+
+def fn_even_odd(ele):
+  if ele%2 == 0:
+    print(ele, " - is even")
+  else:
+    print(ele, " - is odd")
+  
+fn_even_odd(a)
+fn_even_odd(ele=a)
+
+a = [1,2,3,4]
+for ele in a:
+  fn_even_odd(ele=ele)
 
